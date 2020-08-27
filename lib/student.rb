730bc84f939421ad866e683aee572f0a283dbfe1
Christie Yoon <cleex1999@gmail.com>
2020-08-27 05:13:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,8 +3,8 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade, id=nil)
-     @id = id
+  def initialize(name, grade)
+     @id = nil
      @name = name
      @grade = grade
   end
