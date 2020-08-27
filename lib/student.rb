c2a37682354f3209e4ac02c2492c848014f783d2
Christie Yoon <cleex1999@gmail.com>
2020-08-27 05:10:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create(id)
-      student = Student.new(id)
+  def self.create(name, grade)
+      student = Student.new(name, grade)
       student.save
       student
   end
