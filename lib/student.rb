757c1104400eaa92da6aa74378a52cb937754363
Christie Yoon <cleex1999@gmail.com>
2020-08-27 04:43:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,9 +29,10 @@ class Student
 
   def save
     sql = <<-SQL
-
+       INSERT INTO students (name, grade)
+       VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
 end
