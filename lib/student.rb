02aea07260957e072ebca6ba884b012313065a59
Christie Yoon <cleex1999@gmail.com>
2020-08-27 04:57:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,10 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade, id=nil)
+  def initialize(name, grade, id)
      @name = name
      @grade = grade
-     @id = id
+     @id = nil
   end
 
   def self.create_table
